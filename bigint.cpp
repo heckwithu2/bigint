9784f8bd3dbd1272684b8d4e986af1812bfcaf21
heckwithu2 <jheck10@kent.edu>
2021-02-02 17:39:07 -0500

    update

--- bigint.cpp
+++ bigint.cpp
@@ -1,14 +1,46 @@
+#pragma once
 
 /**
-* Jeremiah Heck
-* bigint
+* JEREMIAH HECK
+* BIGINT
 * CS 23001
 */
 
-#include "bigint.hpp"
+#ifndef BIGINT_HPP_
+#define BIGINT_HPP_
 #include <iostream>
 #include <fstream>
 //#include <cstdlib>
+
+
+const int CAPACITY = 400;
+
+
+class bigint {
+private:
+	int number[CAPACITY];
+public:
+	//constructers
+	bigint();
+	bigint(int);
+	bigint(const char[]);
+	bigint(int[]);
+	//methods/friends
+	bigint timesDigit(const int);
+	bigint timesTen(const int);
+	bigint operator*(bigint& B);
+
+	bigint operator=(char []);
+	int operator[] (const int index) const;
+	bigint operator+(bigint&);
+	bool operator==(int v);
+	bool operator==(bigint &x);
+	bool operator==(const char x[]);
+	friend std::ostream& operator<<(std::ostream& os, const bigint& bigint);
+};
+
+
+
 
 bigint::bigint() {
 	for (int i = 0; i < CAPACITY; ++i) {
@@ -230,3 +262,5 @@ bigint bigint::operator*(bigint& B) {
 
 	return product;
 }
+
+#endif
